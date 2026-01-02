f5dc83e5f0d27424733804b572d6f6356ab3a496
youssef khalouk <khaloukyoussef11@gmail.com>
2026-01-02 14:38:57 +0100

    fix recursive function

--- ex5/ft_count_harvest_recursive.py
+++ ex5/ft_count_harvest_recursive.py
@@ -1,4 +1,12 @@
-def ft_count_harvest_recursive():
-    for i in range(1, int(input("Days until harvest: ")) + 1):
-        print(f"Day {i}")
-    print("Harvest time!")
+
+def ft_count_harvest_recursive(days=-1, count=1):
+    if days == -1:
+        days = int(input("Days until harvest: ")) + 1
+    if count < days:
+        print(f"Day {count}")
+        ft_count_harvest_recursive(days, count + 1)
+    if count == days:
+        print("Harvest time!")
+
+
+ft_count_harvest_recursive()
